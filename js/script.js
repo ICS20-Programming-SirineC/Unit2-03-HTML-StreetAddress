ab63930ad94938fb81031a96463924acfc45cfa7
sirinec12345 <122186277+sirinec12345@users.noreply.github.com>
2023-03-12 01:09:12 +0000

    Updated code

--- js/script.js
+++ js/script.js
@@ -8,10 +8,10 @@ function enterClicked () {
   // this function gets user's street name and street number and displays it back to user
 
   // get street name from text field
-  let streetName = document.getElementById("streetName").value;
+  let streetName = document.getElementById("street-name").value;
 
   // get street number from text field and cast it to integer
-  let streetNumber = parseInt(document.getElementById("streetNumber").value);
+  let streetNumber = parseInt(document.getElementById("street-number").value);
 
   // display name and age back to user
   document.getElementById("user-info").innerHTML = "Your street name is " + streetName + " and your street number is " + streetNumber + "."
